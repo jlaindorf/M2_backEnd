564a5eb96350d56f928b913e52e60b2202e60822
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-09-28 21:36:25 -0300

    Aula 04

--- M2A03/variaveis_superglobais.php
+++ M2A03/variaveis_superglobais.php
@@ -1,19 +1,33 @@
-<?php 
+<?php
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
+$method = $_SERVER['REQUEST_METHOD'];
 
-$method = $_SERVER[ 'REQUEST_METHOD'];
+if ($method === 'GET') {
+    $text = file_get_contents('database.txt');
+    $data =  json_decode($text);
 
-if($method === 'GET'){ 
-
-echo  json_encode(['message' => 'Recebi um GET']) ;
-}
-
-else if($method === 'POST'){ 
-
-    echo  json_encode(['message' => 'Recebi um POST']) ;
+    // var_dump($data[0]);
+    echo json_encode(['products' => $data]);
+} else if ($method === "POST") {
+    $body = file_get_contents("php://input");
+    $data = json_decode($body); //acesso ao body da requisição
+    if(isset($data->nome) === false || empty($data->name)){
+        http_response_code(400);
+        echo json_encode(['error' => 'o nome e obrigatorio']);
+        exit; // para a execução 
+        
     }
-    
 
-?>
+    // ler arquivo
+     $text = file_get_contents('database.txt'); // "[]"
+    $arrayItems = json_decode($text); // []
+    $arrayItems[] = $data; // dar push no array
+    
+    file_put_contents('database.txt',json_encode($arrayItems));
+    http_response_code(201);
+    echo json_encode(['message' => 'Recebi um post']);
+}
